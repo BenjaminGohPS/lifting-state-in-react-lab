521a0dfb02a663559a70108196da4971ad10d3df
benjamingohps <benjamingohps@gmail.com>
2024-10-16 13:18:47 +0800

    Lab setup

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// `src/components/BurgerStack.jsx`
+
+const BurgerStack = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default BurgerStack;
---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// src/components/IngredientList.jsx
+
+const IngredientList = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default IngredientList;
